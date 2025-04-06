d25eb853c1712c5a2ae7a266b51b2deae49346d1
Cole <coltonflather@gmail.com>
2025-04-06 19:46:44 -0400

    testingChanges: 1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.post('/api/validateUser', function(req, res) {
-  console.log("req: " + req)
+  console.log("req: " + JSON.stringify(req))
   try{
     if(isValidUser(req.body.username, req.body.password)){
       res.status(200).send(JSON.stringify({"valid": true}));
